bccfb122e635f0383068aa5a7e86133bceac8fbb
fagoon1311 <fs14smailbox65@gmail.com>
2023-12-22 15:26:24 +0530

    cart&redux2

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,21 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import FoodItem from "./FootItems"
+import { clearCart } from "../utils/cartSlice"
 
 const Cart = () => {
     const cartItems = useSelector(store => store.cart.items)
+    const dispatch = useDispatch()
+    const handleClearCart = () =>{
+        dispatch(clearCart())
+    }
 
     return (
     <div>
         <h1 className="text-4xl font-bold text-red-950">Cart items - {cartItems.length}</h1>
-        <FoodItem {...cartItems[0]}/>
+        <button onClick={()=>handleClearCart()} className="bg-slate-300 rounded-md">Clear cart</button>
+        <div className="flex">
+        {cartItems.map(item => <FoodItem {...item}/> )}
+        </div>
     </div>
     )
 }
--- src/components/FootItems.js
+++ src/components/FootItems.js
@@ -1,11 +1,11 @@
 import { IMG_CDN_LINK } from "../Constants"
-const FoodItem = ({name, description, imageId, price}) => {
+const FoodItem = ({name, description, imageId, defaultPrice}) => {
     return (
         <div className="w-56 p-2 m-2 shadow-lg bg-orange-50">
+            <img src={IMG_CDN_LINK+imageId} className="w-56 h-40 p-2"></img>   
             <h2 className="text-red-950 font-bold text-lg">{name}</h2>
             <h4 className="text-xs">{description}</h4>
-            <h4>{price}</h4>   
-            <img src={IMG_CDN_LINK+imageId}></img>   
+            <h4>₹{defaultPrice/100}</h4>   
         </div>
     )
 }
